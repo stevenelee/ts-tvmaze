d099b98ad2066ae8f747bac8bc6b122977143053
Steven Lee <stevelee83@gmail.com>
2023-06-05 14:43:18 -0700

    work on searchShowsByTerm

--- tvmaze.ts
+++ tvmaze.ts
@@ -5,6 +5,15 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
+const DEFAULT_IMAGE_URL: string = "https://tinyurl.com/tv-missing"
+const TVMAZE_API_URL: string = "https://api.tvmaze.com/search"
+
+interface ShowInterface {
+  id: number;
+  name: string;
+  summary: string;
+  image: string | null;
+}
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -13,27 +22,14 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+async function searchShowsByTerm(term: string): Promise<ShowInterface[]> {
+  const resp = await axios.get(`${TVMAZE_API_URL}/shows?q=${term}`)
+
+  shows.map((show: ShowInterface) => show.image === null ? show.image = DEFAULT_IMAGE_URL
+                                                         : show.image = show.image);
+  return shows;
 }
+
 
 
 /** Given list of shows, create markup for each and to DOM */
@@ -70,7 +66,7 @@ function populateShows(shows) {
 
 async function searchForShowAndDisplay() {
   const term = $("#searchForm-term").val();
-  const shows = await getShowsByTerm(term);
+  const shows = await searchShowsByTerm(term);
 
   $episodesArea.hide();
   populateShows(shows);
